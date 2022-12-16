155d95202662f179e99962020eab7a4f6d7888e8
김두나 <110874398+rlaensk@users.noreply.github.com>
2022-12-16 23:21:25 +0900

    ..

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-$(function(){
-  $('.country_toggle').click(function(){
-    $('.dropdown_menu').css('display','block');
-  });
-});
+
+
+
+//index.js
+
+let slider = document.querySelector("#slider_box")
+let innerSlider = document.querySelector("#offer_box")
+let pressed = false
+let startx
+let x
+
+slider.addEventListener("mousedown", e => {
+  pressed = true
+  startx = e.offsetX - innerSlider.offsetLeft
+  slider.style.cursor = "grabbing"
+})
+
+slider.addEventListener("mouseenter", () => {
+  slider.style.cursor = "grab"
+})
+
+slider.addEventListener("mouseup", () => {
+  slider.style.cursor = "grab"
+})
+
+window.addEventListener("mouseup", () => {
+  pressed = false
+})
+
+slider.addEventListener("mousemove", e => {
+  if (!pressed) return
+  e.preventDefault()
+  x = e.offsetX
+
+  innerSlider.style.left = `${x - startx}px`
+  checkboundary()
+})
+
+function checkboundary() {
+  let outer = slider.getBoundingClientRect()
+  let inner = innerSlider.getBoundingClientRect()
+
+  if (parseInt(innerSlider.style.left) > 0) {
+    innerSlider.style.left = "0px"
+  } else if (inner.right < outer.right) {
+    innerSlider.style.left = `-${inner.width - outer.width}px`
+  }
+}
