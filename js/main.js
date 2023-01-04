bd9a240532e72730a5b8db626bb899199e1c2f7a
김두나 <110874398+rlaensk@users.noreply.github.com>
2023-01-04 17:12:58 +0900

    2341

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // scroll하면 헤더배경색 변하기
-$(function(){
-  let isOn=false;
-  $(window).scroll(function(){
-    if($(this).scrollTop()>100){
-      if(!isOn){
+$(function () {
+  let isOn = false;
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      if (!isOn) {
         $('header').addClass('on');
-        isOn=true;
+        isOn = true;
       }
-    }else{
-      if(isOn){
+    } else {
+      if (isOn) {
         $('header').removeClass('on');
-        isOn=false;
+        isOn = false;
       }
     }
   });
@@ -27,10 +27,10 @@ menuTrigger.addEventListener('click', (event) => {
 
 
 
-$('.plus').click(function(){
+$('.plus').click(function () {
   $(this).not('.plus').removeClass(' active').parent().next().slideUp();
   $(this).toggleClass(' active').parent().next().slideToggle();
-  
+
 });
 
 
@@ -42,16 +42,16 @@ $(function () {
     if ($('#menu_trigger').css('width') == '0px') {
       $('#menu_trigger').css('width', '100%');
 
-      $('.menu-trigger span').css('background-color','rgb(128,128,128)');
-      $('.logo_text>p').css('color','rgb(128,128,128)');
-      $('.country_toggle span').css('color','rgb(128,128,128)')
-      $('.dropdown_label').css('color','rgb(128,128,128)')
-      $('#book_tap a').css({'color':'rgb(128,128,128)'});
-      $('#book_tap').css('border','1px solid rgb(128,128,128)');
-      $('.dropdown_menu').css('box-shadow','0 0 8px #99a1a7 ');
-      $('.cstoggle+ul').css('box-shadow','0 0 8px #99a1a7 ');
+      $('.menu-trigger span').css('background-color', 'rgb(128,128,128)');
+      $('.logo_text>p').css('color', 'rgb(128,128,128)');
+      $('.country_toggle span').css('color', 'rgb(128,128,128)')
+      $('.dropdown_label').css('color', 'rgb(128,128,128)')
+      $('#book_tap a').css({ 'color': 'rgb(128,128,128)' });
+      $('#book_tap').css('border', '1px solid rgb(128,128,128)');
+      $('.dropdown_menu').css('box-shadow', '0 0 8px #99a1a7 ');
+      $('.cstoggle+ul').css('box-shadow', '0 0 8px #99a1a7 ');
 
-       $('#about_us').css('transform', 'translateY(0px)').css('opacity', '1');
+      $('#about_us').css('transform', 'translateY(0px)').css('opacity', '1');
       $('#accom').css('transform', 'translateY(0px)').css('opacity', '1');
 
       $('#special').css('transform', 'translateY(0px)').css('opacity', '1');
@@ -61,19 +61,19 @@ $(function () {
       $('#community').css('transform', 'translateY(0px)').css('opacity', '1');
 
       $('#reservation').css('transform', 'translateY(0px)').css('opacity', '1');
-     
-      $('.side_footer').css('opacity','1');
-     
+
+      $('.side_footer').css('opacity', '1');
+
     } else {
-   
-      $('.logo_text>p').css('color','#fff');
-      $('.menu-trigger span').css('background-color','#fff');
-      $('.country_toggle span').css('color','#fff');
-      $('.dropdown_label').css('color','#fff');
-      $('#book_tap a').css('color','#fff');
-      $('#book_tap').css('border','1px solid #fff');
-      $('.dropdown_menu').css('box-shadow','none ');
-      $('.cstoggle+ul').css('box-shadow','none');
+
+      $('.logo_text>p').css('color', '#fff');
+      $('.menu-trigger span').css('background-color', '#fff');
+      $('.country_toggle span').css('color', '#fff');
+      $('.dropdown_label').css('color', '#fff');
+      $('#book_tap a').css('color', '#fff');
+      $('#book_tap').css('border', '1px solid #fff');
+      $('.dropdown_menu').css('box-shadow', 'none ');
+      $('.cstoggle+ul').css('box-shadow', 'none');
       $('#about_us').css('transform', 'translateY(100px)').css('opacity', '0');
       $('#accom').css('transform', 'translateY(100px)').css('opacity', '0');
 
@@ -84,8 +84,8 @@ $(function () {
       $('#community').css('transform', 'translateY(100px)').css('opacity', '0');
 
       $('#reservation').css('transform', 'translateY(100px)').css('opacity', '0');
-      
-      $('.side_footer').css('opacity','0');
+
+      $('.side_footer').css('opacity', '0');
       $('#menu_trigger').css({ 'width': '0px' });
 
     }
@@ -95,138 +95,138 @@ $(function () {
 
 
 
-  // EU
+// EU
 
-  $(function () {
-    let btn = $(".country_toggle");
-    let layer = $('.dropdown_menu');
+$(function () {
+  let btn = $(".country_toggle");
+  let layer = $('.dropdown_menu');
 
-    btn.click(function () {
+  btn.click(function () {
     layer.slideToggle();
-   
+
   });
 });
 
 
 
-  
 
-  // cs
-  $(function () {
-    let btn1 = $('.cstoggle');
-    let layer1 = $('.csdropdown');
 
-    btn1.click(function () {
-      layer1.slideToggle();
-    });
+// cs
+$(function () {
+  let btn1 = $('.cstoggle');
+  let layer1 = $('.csdropdown');
+
+  btn1.click(function () {
+    layer1.slideToggle();
   });
+});
 
 
-  // hotel room
-  $('.room_').not(':first').css('display','none');
+// hotel room
+$('.room_').not(':first').css('display', 'none');
 
-  $('.next_1').click(function () {
-    $('.room_img:first').insertAfter('.room_img:last');
-    $('.room_img_box').css('margin-left', -50.5);
-    $('.room_img_box').stop().animate({ marginLeft: 0 }, 1000);
-    
-    $('.room_:first').insertAfter('.room_:last');
-    $('.room_:first').css('display','block');
-    $('.room_').not(':first').css('display','none');
-    $('.room_').css('margin-left',50);
-    $('.room_').stop().animate({marginLeft:0},1000);
+$('.next_1').click(function () {
+  $('.room_img:first').insertAfter('.room_img:last');
+  $('.room_img_box').css('margin-left', -50.5);
+  $('.room_img_box').stop().animate({ marginLeft: 0 }, 1000);
 
-      // $('.room_:first').insertAfter('.room_:last');
+  $('.room_:first').insertAfter('.room_:last');
+  $('.room_:first').css('display', 'block');
+  $('.room_').not(':first').css('display', 'none');
+  $('.room_').css('margin-left', 50);
+  $('.room_').stop().animate({ marginLeft: 0 }, 1000);
 
-  
-  });
-
-  $('.next_2').click(function () {
-    $('.room_img:last').insertBefore('.room_img:first');
-    $('.room_img_box').css('margin-left', 50.5);
-    $('.room_img_box').stop().animate({ marginLeft: 0 }, 1000);
-
-    $('.room_:last').insertBefore('.room_:first');
-    $('.room_:first').css('display','block');
-    $('.room_').not(':first').css('display','none');
-    $('.room_').css('margin-left', 50.5);
-    $('.room_').stop().animate({ marginLeft: 0 }, 1000);
-    });
- 
+  // $('.room_:first').insertAfter('.room_:last');
 
 
+});
 
-  //index.js
+$('.next_2').click(function () {
+  $('.room_img:last').insertBefore('.room_img:first');
+  $('.room_img_box').css('margin-left', 50.5);
+  $('.room_img_box').stop().animate({ marginLeft: 0 }, 1000);
 
-  let slider = document.querySelector("#slider_box")
-  let innerSlider = document.querySelector("#offer_box")
-  let pressed = false
-  let startx
-  let x
-
-  slider.addEventListener("mousedown", e => {
-    pressed = true
-    startx = e.offsetX - innerSlider.offsetLeft
-    slider.style.cursor = "grabbing"
-  })
-
-  slider.addEventListener("mouseenter", () => {
-    slider.style.cursor = "grab"
-  })
-
-  slider.addEventListener("mouseup", () => {
-    slider.style.cursor = "grab"
-  })
+  $('.room_:last').insertBefore('.room_:first');
+  $('.room_:first').css('display', 'block');
+  $('.room_').not(':first').css('display', 'none');
+  $('.room_').css('margin-left', 50.5);
+  $('.room_').stop().animate({ marginLeft: 0 }, 1000);
+});
 
 
 
-  window.addEventListener("mouseup", () => {
-    pressed = false
-  })
 
-  slider.addEventListener("mousemove", e => {
-    if (!pressed) return
-    e.preventDefault()
-    x = e.offsetX
+//index.js
 
-    innerSlider.style.left = `${x - startx}px`
-    checkboundary()
-  })
+let slider = document.querySelector("#slider_box")
+let innerSlider = document.querySelector("#offer_box")
+let pressed = false
+let startx
+let x
 
-  function checkboundary() {
-    let outer = slider.getBoundingClientRect()
-    let inner = innerSlider.getBoundingClientRect()
+slider.addEventListener("mousedown", e => {
+  pressed = true
+  startx = e.offsetX - innerSlider.offsetLeft
+  slider.style.cursor = "grabbing"
+})
 
-    if (parseInt(innerSlider.style.left) > 0) {
-      innerSlider.style.left = "0px"
-    } else if (inner.right < outer.right) {
-      innerSlider.style.left = `-${inner.width - outer.width}px`
-    }
+slider.addEventListener("mouseenter", () => {
+  slider.style.cursor = "grab"
+})
+
+slider.addEventListener("mouseup", () => {
+  slider.style.cursor = "grab"
+})
+
+
+
+window.addEventListener("mouseup", () => {
+  pressed = false
+})
+
+slider.addEventListener("mousemove", e => {
+  if (!pressed) return
+  e.preventDefault()
+  x = e.offsetX
+
+  innerSlider.style.left = `${x - startx}px`
+  checkboundary()
+})
+
+function checkboundary() {
+  let outer = slider.getBoundingClientRect()
+  let inner = innerSlider.getBoundingClientRect()
+
+  if (parseInt(innerSlider.style.left) > 0) {
+    innerSlider.style.left = "0px"
+  } else if (inner.right < outer.right) {
+    innerSlider.style.left = `-${inner.width - outer.width}px`
   }
+}
 
 
 
-  // review 무한반복
-  $('.next_3').click(function () {
-    $('.review_1 .review_img:last').prependTo('.review_1');
-    $('.review_1').css('margin-left', -400);
-    $('.review_1').stop().animate({ marginLeft: 0 }, 800);
-  });
+// review 무한반복
+$('.next_3').click(function () {
+  $('.review_1 .review_img:last').prependTo('.review_1');
+  $('.review_1').css('margin-left', -400);
+  $('.review_1').stop().animate({ marginLeft: 0 }, 800);
+});
 
-  $('.next_4').click(function () {
-    $('.review_1').stop().animate({ marginLeft: -400 }, 800, function () {
-      $('.review_1 .review_img:first').appendTo('.review_1');
-      $('.review_1').css({ marginLeft: 0 });
-    })
-  });
+$('.next_4').click(function () {
+  $('.review_1').stop().animate({ marginLeft: -400 }, 800, function () {
+    $('.review_1 .review_img:first').appendTo('.review_1');
+    $('.review_1').css({ marginLeft: 0 });
+  })
+});
 
 // 반응형 자바
-  let mal=window.matchMedia('screen and (max-width:1230px)');
-  if(mal.matches){
-    $('#about_line>p').text("Pool Villa Room");
-    $('#inpnity>p').text('Infinity&Rooftop Pool');
-    $('#BBQ>p').text('Breakfast&BBQ');
-  }
+let mal = window.matchMedia('screen and (max-width:1230px)');
+if (mal.matches) {
+  $('#about_line>p').text("Pool Villa Room");
+  $('#inpnity>p').text('Infinity&Rooftop Pool');
+  $('#BBQ>p').text('Breakfast&BBQ');
+}
 
 
   // $(function(){
